c94b41d4fd420c15a3ea73f50c54ab0ceecf6837
fredrikberzins <fredrik.berzins@gmail.com>
2023-09-14 13:14:09 +0200

    fix low coyp merge

--- Sorting/sort.java
+++ Sorting/sort.java
@@ -71,23 +71,19 @@ public class sort {
             return;
         }
         int[] aux = Inital.copyArr(array);
-        sort_lowCopyMerge(aux, array, 0, array.length -1);
+        sort_lowCopyMerge(array, aux, 0, array.length -1);
         return;
     }
 
-    private static int[] sort_lowCopyMerge(int[] array, int[] aux, int lo, int hi) {
+    private static void sort_lowCopyMerge(int[] array, int[] aux, int lo, int hi) {
         if (lo != hi) {
             int mid = (lo + hi)/2;
-            aux = sort_lowCopyMerge(array, aux, lo, mid);
-            aux = sort_lowCopyMerge(array, aux, mid + 1, hi);
+            sort_lowCopyMerge(aux, array, lo, mid);
+            sort_lowCopyMerge(aux, array, mid + 1, hi);
             merge_lowCopyMerge(array, aux, lo, mid, hi);
         }
-        return array;
     }
     private static void merge_lowCopyMerge(int[] array, int[] aux, int lo, int mid, int hi) {
-        for (int i = lo; i <= hi; i++) {
-            aux[i] = array[i];
-        }
         int i = lo;
         int j = mid+1;
         for ( int k = lo; k <= hi; k++) {
