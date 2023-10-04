fd4853669f26632d1a901992ad10e9da78d09cea
fredrikberzins <fredrik.berzins@gmail.com>
2023-10-04 18:47:56 +0200

    sort almost done issue with first

--- C_Quick/Quick_Benchmark.java
+++ C_Quick/Quick_Benchmark.java
@@ -1,32 +1,15 @@
 public class Quick_Benchmark {
     public static void main(String[] arg) {
-	int[] sizes = {5,10/*100,200,100,200,100,200,400,800,1600,3200,6400*/};
+	int[] sizes = {10/*100, 200, 100, 200, 100, 200, 400, 800, 1600, 3200, 6400*/};
 		System.out.printf("those: # searching through an array of length n, time in ns\n");
 		System.out.printf("#%4s%15s%15s\n", "n", "Array", "LinkedList");
 		for ( int n : sizes) {
 			System.gc();
 			int loop = 2;
-			int k = 2;
+			int k = 1;
 
 			int[][] Arr = Quick_Inital.Unsorted2d(loop, n);
 			Quick_LinkedList[] LinkList = Quick_LinkedList.LinkedListArr(Arr);
-
-			System.out.println("prerun: ");
-			for (int m = 0; m < loop; m++) {
-				System.out.println("begin array: ");
-				for (int j = 0; j < n; j++) {
-					System.out.println("valeu: " + Arr[m][j]);
-				}
-			}
-
-			for (int m = 0; m < loop; m++) {
-					System.out.println("begin list: ");
-					Quick_Node qn = LinkList[m].first;
-					while (qn != null) {
-						System.out.println("valeu: " + qn.value);
-						qn = qn.next;
-					}
-				}
 
 			System.out.printf("%5d", n);
 
@@ -40,37 +23,30 @@ public class Quick_Benchmark {
 				long t1 = System.nanoTime();
 				double t = (t1 - t0);
 				if (t < min) min = t;
-				System.out.println("rerun: ");
-				for (int m = 0; m < loop; m++) {
-					System.out.println("new array: ");
-					for (int j = 0; j < n; j++) {
-						System.out.println("valeu: " + ArrCopy[m][j]);
-					}
-				}
 			}
-			System.out.printf("%10.0f(%3.1f)" , (min/(loop*1000)),((min/(loop))/(10*n*Math.log(n))));
+			//System.out.printf("%10.0f(%3.1f)" , (min/(loop)),((min/(loop))/(10*n*Math.log(n))));
 
 			min = Double.POSITIVE_INFINITY;
 			for (int i = 0; i < k; i++) {
 				Quick_LinkedList[] LinkListCopy = LinkList;
 				long t0 = System.nanoTime();
 				for (int m = 0; m < loop; m++) {
-					Quick_LinkedList.QuickSortList(LinkListCopy[m].first, LinkListCopy[m].end);
+					Quick_LinkedList.QuickSortList(LinkListCopy[m].first, LinkListCopy[m].last);
 				}
 				long t1 = System.nanoTime();
 				double t = (t1 - t0);
 				if (t < min) min = t;
-				System.out.println("rerun: ");
+				System.out.println("list after: ");
 				for (int m = 0; m < loop; m++) {
-					System.out.println("new list: ");
+					System.out.println("  new list: ");
 					Quick_Node qn = LinkListCopy[m].first;
 					while (qn != null) {
-						System.out.println("valeu: " + qn.value);
+						System.out.println("    valeu: " + qn.value);
 						qn = qn.next;
 					}
 				}
 			}
-			System.out.printf("%10.0f(%3.1f)\n" , (min/(loop*1000)),((min/(loop))/(n*n)));	
+			//System.out.printf("%10.0f(%3.1f)\n" , (min/(loop)),((min/(loop))/(n*n)));	
 		}
     }
 }
--- C_Quick/Quick_LinkedList.java
+++ C_Quick/Quick_LinkedList.java
@@ -1,25 +1,24 @@
 public class Quick_LinkedList {
 
-    public static Quick_Node first;
-    public static Quick_Node last;
+    public Quick_Node first;
+    public Quick_Node last;
 
-    public Quick_LinkedList(int[] Arr) {
-        first = new Quick_Node(Arr[0]);
-        Quick_Node n = first;
-		for (int i = 1; i < Arr.length; i++) {
-			n.next = new Quick_Node(Arr[i]);
-			n = n.next;
-		}
-        last = n;
+    public Quick_LinkedList() {
+        this.first = new Quick_Node();
+        Quick_Node n = this.first;
+        this.last = n;
     }
 
     public static Quick_LinkedList[] LinkedListArr(int[][] Arr) {
         Quick_LinkedList[] List2d = new Quick_LinkedList[Arr.length];
         for (int i = 0; i < Arr.length; i++) {
-            List2d[i].first = new Quick_Node(Arr[i][0]);
-            Quick_Node n = first;
+            List2d[i] = new Quick_LinkedList();
+            Quick_Node n = new Quick_Node();
+            n = List2d[i].first;
+            n.value = Arr[i][0];
             for (int m = 1; m < Arr[i].length; m++) {
-                n.next = new Quick_Node(Arr[i][m]);
+                n.next = new Quick_Node();
+                n.next.value = Arr[i][m];
                 n = n.next;
             }
             List2d[i].last = n;
@@ -28,22 +27,21 @@ public class Quick_LinkedList {
     }
 
     public static void QuickSortList(Quick_Node lo, Quick_Node hi) {
-        Quick_Node pivotNode = lo;
-
-        if ( lo != hi) {
-            int pivot = hi.value;
+        if (lo != hi) {
+            int pivotValue = hi.value;
             Quick_Node curr = lo.next;
+            Quick_Node pivot = lo;
 
             while (curr != hi) {
-                if (curr.value <= pivot) {
-                    pivotNode = pivotNode.next;
-                    swap(pivotNode, curr);
+                if (curr.value <= pivotValue) {
+                    pivot = pivot.next;
+                    swap(pivot, curr);
                 }
                 curr = curr.next;
             }
-            swap(hi, pivotNode);
-            QuickSortList(lo, pivotNode);
-            QuickSortList(pivotNode.next, hi);
+            swap(pivot.next, hi);
+            QuickSortList(lo, pivot);
+            QuickSortList(pivot.next, hi);
         }
     }
     public static void swap(Quick_Node a, Quick_Node b) {
@@ -51,5 +49,4 @@ public class Quick_LinkedList {
         a.value = b.value;
         b.value = temp;
     }
-
 }
